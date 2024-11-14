56a38ad3dd7fa71b51576f88de837060a3b47ec5
gmelo138 <138888978+gmelo138@users.noreply.github.com>
2024-11-14 16:04:17 -0300

    updating app.js

--- server/src/app.js
+++ server/src/app.js
@@ -6,8 +6,14 @@ const { errorHandler } = require('./middleware/errorHandler');
 const app = express();
 connectDB();
 
+// Route files
+const [entity]Routes = require('./routes/[entity]Routes');
+
 app.use(cors());
 app.use(express.json());
+
+// Mount routers
+app.use('/api/v1/users', [entity]Routes);
 
 app.use(errorHandler);
 
